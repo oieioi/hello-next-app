f93118d1fa3a1fe5eab257928b06f0cea6a14c9a
oieioi <atsuinatsu.samuifuyu@gmail.com>
2021-01-14 16:58:04 +0900

    Add prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Index() {
-  return <div>index</div>;
+import type { GetServerSideProps } from 'next';
+import prisma from '../lib/prisma';
+
+type Props = {
+  count: number;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const count = await prisma.user.count();
+  return {
+    props: {
+      count,
+    }
+  }
+}
+
+export default function Index(props: Props) {
+  return <div>user count: {props.count}</div>;
 }
